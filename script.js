257e8770cba83da82f065d8eed49eca23c43eca5
Angela McLeary <angela.mcleary@outlook.com>
2026-04-09 17:23:00 +0100

    generate all episodes and seasons

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
+  episodeList.forEach((episodes) => {
+    const paragraph = document.createElement("h2");
+    paragraph.textContent = `S${episodes.seasons}E${episodes.number}: ${episodes.name}`;
+    rootElem.appendChild(paragraph);
+
+    const image = document.createElement("img");
+    image.src = episodes.image.medium;
+    rootElem.appendChild(image);
+
+    const summary = document.createElement ("summary")
+  });
+}
 window.onload = setup;
